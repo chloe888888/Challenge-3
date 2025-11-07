18f13fa76b8de80647e9b3f980c048b2efa39c40
chloe <lin_xuan_en_chloe@students.edu.sg>
2025-11-07 10:48:54 +0800

    Initial Commit

---
+++ Challenge 3/Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3App.swift
+//  Challenge 3
+//
+//  Created by Chloe Lin on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
